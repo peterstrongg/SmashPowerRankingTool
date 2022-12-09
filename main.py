7189ef8d5b54b6f178b5561e64c3572a22441b9b
peterstrongg <peterstrong8343@gmail.com>
2022-12-08 23:13:29 -0500

    began excel file population

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pysmashgg
 from tabulate import tabulate
 from time import sleep
+import xlsxwriter
+import os
+
 import Player
 
 KEY = pysmashgg.SmashGG('2429ea2ccddc1718cc121120d210722e', True)
-ATTENDANCE_REQ = 4
+FILE_NAME = 'single.txt'
+ATTENDANCE_REQ = 1
 
 def getElligiblePlayers(tournaments):
     players = []
@@ -60,16 +64,20 @@ def getH2H(players, tournaments):
                     winner = set['entrant2Players'][0]['playerTag']
                     loser = set['entrant1Players'][0]['playerTag']
 
-                if winner in player.h2h.keys() and loser in player.h2h.keys():
-                    for player in players:
-                        if player.tag == winner:
-                            if winner in player.h2h:
-                                player.h2h[loser][0] = player.h2h[loser][0]+1
-                        elif player.tag == loser:
-                            if loser in player.h2h:
-                                player.h2h[winner][1] = player.h2h[winner][1]+1
-                    # end for
+                if winner not in players[0].h2h.keys() or loser not in players[0].h2h.keys():
+                    page += 1
+                    sets = KEY.tournament_show_sets(tournament, 'ultimate-singles', page)
+                    break
                 # end if
+
+                for player in players:
+                    if player.tag == winner:
+                        if winner in player.h2h:
+                            player.h2h[loser][0] = player.h2h[loser][0]+1
+                    elif player.tag == loser:
+                        if loser in player.h2h:
+                            player.h2h[winner][1] = player.h2h[winner][1]+1
+                # end for
             # end for
             page += 1
             sets = KEY.tournament_show_sets(tournament, 'ultimate-singles', page)
@@ -90,10 +98,33 @@ def printElligiblePlayers(players):
     print(tabulate(table, headers = ["Players", "Attendance"]))
 # end def
 
+def writeToExcel(players):
+    try:
+        os.remove('H2HData.xlsx')
+    except:
+        print("xlsx file could not be removed")
+
+    wbk = xlsxwriter.Workbook('H2HData.xlsx')
+    sheet = wbk.add_worksheet()
+
+    sheet.set_column(0, 0, 25)
+    sheet.set_row(0, 25)
+
+    row = 1
+    for i in range(len(players)):
+        sheet.set_row(i+1, 30)
+        sheet.write(row, 0, players[i].tag)
+        row += 1
+    #sheet.write('B1', 'Test')
+
+    wbk.close()
+# end def
+
 if __name__ == "__main__":
-    with open("events.txt") as file:
+    with open(FILE_NAME) as file:
         tournaments = file.read().splitlines()
 
     players = getElligiblePlayers(tournaments)
     players = getH2H(players, tournaments)
     printElligiblePlayers(players)
+    writeToExcel(players)
